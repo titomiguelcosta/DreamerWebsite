a11d757b80ebad46bb3b41053ad7298546ed321e
Tito Miguel Costa <github@titomiguelcosta.com>
2012-09-08 16:24:49 +0100

    acertos

--- config/routing.php
+++ config/routing.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app->get('/', function () use ($app) {
     return $app['twig']->render('home.twig', array(
     ));
--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../config/settings.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
@@ -13,6 +14,9 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider(), array(
 $app->register(new Silex\Provider\FormServiceProvider(), array(
 ));
 $app->register(new Silex\Provider\ValidatorServiceProvider(), array(
+));
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallback' => 'en',
 ));
 
 require_once __DIR__.'/../config/routing.php';
