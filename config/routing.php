5533ac65d3a66cfe310b339742aa4013f2bd9200
Tito Miguel Costa <github@titomiguelcosta.com>
2012-11-26 12:27:12 +0000

    added routes requirements

--- config/routing.php
+++ config/routing.php
@@ -7,13 +7,14 @@ $app->get('/project/{slug}', 'TitoMiguelCosta\Controller\Site::projectAction')->
 $app->match('/contacts', 'TitoMiguelCosta\Controller\Site::contactAction')->bind('contact');
 $app->get('/code', 'TitoMiguelCosta\Controller\Site::codeAction')->bind('code');
 
-$app->get('/blog/{page}', 'TitoMiguelCosta\Controller\Blog::listAction')->value('page', '1')->bind('blog_list');
+$app->get('/blog/{page}', 'TitoMiguelCosta\Controller\Blog::listAction')->assert('page', '\d+')->value('page', '1')->bind('blog_list');
 $app->get('/blog/{slug}', 'TitoMiguelCosta\Controller\Blog::postAction')->bind('blog_post');
 $app->get('/blog/{category}/category', 'TitoMiguelCosta\Controller\Blog::categoryAction')->bind('blog_category');
 
-$app->get('/music/{page}', 'TitoMiguelCosta\Controller\Music::listAction')->value('page', '1')->bind('music');
+$app->get('/music/{page}', 'TitoMiguelCosta\Controller\Music::listAction')->assert('page', '\d+')->value('page', '1')->bind('music');
 
-$app->get('/photos/{page}', 'TitoMiguelCosta\Controller\Photo::listAction')->value('page', '1')->bind('photos');
+$app->get('/photos/{page}', 'TitoMiguelCosta\Controller\Photo::listAction')->assert('page', '\d+')->value('page', '1')->bind('photos');
 
+/** auxiliar routes */
 $app->get('/youtube/auth', 'TitoMiguelCosta\Controller\Main::youtubeAuthAction')->bind('youtube_auth');
 $app->get('/youtube/token', 'TitoMiguelCosta\Controller\Main::youtubeTokenAction')->bind('youtube_token');
